5604e11395630cdf3b49fe5607872749d6deb583
michaeljluo <51395564+michaeljluo@users.noreply.github.com>
2021-03-02 03:58:21 -0500

    new table

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import './Main.css';
-
-// Table from react-bootstrap
-import { Table } from "react-bootstrap";
-// Bootstrap CSS
+import { Divider } from '@material-ui/core';
 import "bootstrap/dist/css/bootstrap.min.css";
-// To make rows collapsible
-import "bootstrap/js/src/collapse.js";
 
+
+import { Container, Row, Col } from 'reactstrap';
 
 export const Main = () => (
   
@@ -16,45 +13,18 @@ export const Main = () => (
     <h1>Chi Yuan Huang Law Office Home Page</h1>
     <h2 className="subheader">Case List</h2>
     <div className="caseList">
-      <Table striped bordered hover>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr
-                data-toggle="collapse"
-                data-target=".multi-collapse1"
-                aria-controls="multiCollapseExample1"
-            >
-                <td>1</td>
-                <td>TEST 123</td>
-                <td>test123@test.com</td>
-            </tr>
-            <tr class="collapse multi-collapse1" id="multiCollapseExample1">
-                <td>Child col 1</td>
-                <td>Child col 2</td>
-                <td>Child col 3</td>
-            </tr>
-            <tr
-                data-toggle="collapse"
-                data-target=".multi-collapse2"
-                aria-controls="multiCollapseExample2"
-            >
-                <td>2</td>
-                <td>TEST 456</td>
-                <td>test456@test.com</td>
-            </tr>
-            <tr class="collapse multi-collapse2" id="multiCollapseExample2">
-                <td>Child col 1</td>
-                <td>Child col 2</td>
-                <td>Child col 3</td>
-            </tr>
-        </tbody>
-      </Table>
+        <Divider/>
+        <Container>
+            <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+            </Row>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
     </div>
     Main Component
   </div>
