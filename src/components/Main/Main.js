a3ba3adc01d036d8936b6d467650a9308dc5b133
michaeljluo <51395564+michaeljluo@users.noreply.github.com>
2021-04-21 19:51:05 -0400

    open and close dialog

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
+import CaseDialog from '../CaseDialog/CaseDialog'
+
 import { Typography } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
+
 import { Container } from "react-bootstrap";
 
 
@@ -51,9 +43,7 @@ const columns = [
     { id:2, number: "RE-Q-002", status: "Closed", address: '93 2nd Ave' }
   ];
 
-  const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-  });
+
 
 export default function Main() {
   const classes = useStyles();
@@ -61,14 +51,9 @@ export default function Main() {
   const [selectionModel, setSelectionModel] = React.useState([]);
   const [open, setOpen] = React.useState(false);
 
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const onClose = React.useCallback(open => {
+    setOpen(open);
+  });
 
 
 
@@ -94,30 +79,8 @@ export default function Main() {
         
         {//use selectionModel here
         }
-        <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-          <AppBar className={classes.appBar}>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-                <CloseIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                Sound
-              </Typography>
-              <Button autoFocus color="inherit" onClick={handleClose}>
-                save
-              </Button>
-            </Toolbar>
-          </AppBar>
-          <List>
-            <ListItem button>
-              <ListItemText primary="Phone ringtone" secondary="Titania" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-            </ListItem>
-          </List>
-        </Dialog>
+        <CaseDialog open={open} onClose={onClose}></CaseDialog>
+
       </Container>
     </div>
   );
